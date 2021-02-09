81a51f7771eeb9c42f644ade0f92e24310148a5a
binghongli <b97607065@gmail.com>
2021-02-09 15:04:25 +0000

    004 finished

--- 004_flask_integrate_firestore/src/app.py
+++ 004_flask_integrate_firestore/src/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 from flask import Flask, request, abort
 from src.controllers.user_controller import UserController
 from flask_cors import CORS
